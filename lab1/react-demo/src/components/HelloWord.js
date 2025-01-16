63b964197a0be7692c7a88407c7d6022ed855b86
thanhtungf123 <thanhtungphankt@gmail.com>
2025-01-16 13:15:04 +0700

    commit

--- lab1/react-demo/src/components/HelloWord.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// Một component HelloWorld linh hoạt hơn
-function HelloWorld({ name }) {
-  return <div>Hello, {name || 'World'}!</div>;
-}
-
-export default HelloWorld;
